1e45dcd639edd3a6fd465e677ac49fbef789f81c
Uygar Süslü <uygarsuslu1@gmail.com>
2022-02-10 18:18:17 +0300

    Intro to Conditional Statements added

--- 10-Days-of-JavaScript.js
+++ 10-Days-of-JavaScript.js
@@ -55,3 +55,13 @@ function getPerimeter(length, width) {
 }
 
 // **************************************************************************************************** //
+
+// ********** Day 3: Intro to Conditional Statements ********** //
+
+function main() {
+    const N = parseInt(readLine().trim(), 10);
+    if(N % 2 == 1) {console.log("Weird")}
+    if(N % 2 == 0 && N >= 2 && N <= 5) {console.log("Not Weird")}
+    if(N % 2 == 0 && N >= 6 && N <= 20) {console.log("Weird")}
+    if(N % 2 == 0 && N > 20) {console.log("Not Weird")}
+}
